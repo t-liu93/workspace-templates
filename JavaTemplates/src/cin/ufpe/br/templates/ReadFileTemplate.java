224bba89f6392e0bd76acaa5737445959d37a736
Felipe Ebert <felipe.ebert@gmail.com>
2016-12-23 15:39:35 +0100

    Updated ReadFile from Java 8 to include read per line.

--- JavaTemplates/src/cin/ufpe/br/templates/ReadFileTemplate.java
+++ JavaTemplates/src/cin/ufpe/br/templates/ReadFileTemplate.java
@@ -5,17 +5,26 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class ReadFileTemplate {
 
 	public static void main(String[] args) {
 
 		/*********** JAVA 8 ***************/
+		// read the whole file
 		try {
 			String content = new String(Files.readAllBytes(Paths.get("./codereviews-android/103241.json")));
 			System.out.println(content);
-		} catch (IOException e1) {
-			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// read each line from the file
+		try {
+			List<String> content = Files.readAllLines(Paths.get("./codereviews-android/103241.json"));
+			System.out.println(content);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		
 		/*********** JAVA 6- ***************/
